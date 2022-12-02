cc42045847083bc4eefe00e98b6a333e1dbb61e6
LegameMc <legamemc@gmail.com>
2022-12-02 11:53:17 +0800

    Optimized Day 1 challenge code

--- src/main/java/com/gmail/legamemc/adventofcode2022/questions/Day1.java
+++ src/main/java/com/gmail/legamemc/adventofcode2022/questions/Day1.java
@@ -3,17 +3,14 @@ package com.gmail.legamemc.adventofcode2022.questions;
 import com.gmail.legamemc.adventofcode2022.Challenge;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Day1 implements Challenge {
 
 
     @Override
     public void execute() throws Exception{
-        int[] arr = new int[3];
+        int[] tops = new int[3];
         InputStream inputStream = getClass().getResourceAsStream("/Day1Input.txt");
 
         if(inputStream == null){
@@ -29,7 +26,7 @@ public class Day1 implements Challenge {
 
         while((line = bufferedReader.readLine()) != null){
             if(line.isEmpty()){
-                insertTop3(arr, sum);
+                adjustTop(tops, sum);
                 sum = 0;
                 continue;
             }
@@ -38,28 +35,27 @@ public class Day1 implements Challenge {
         }
 
         if(sum != 0){
-            insertTop3(arr, sum);
+            adjustTop(tops, sum);
         }
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.stream(arr).sum());
+        System.out.println(Arrays.toString(tops));
+        System.out.println(Arrays.stream(tops).sum());
     }
 
-    private void insertTop3(int[] arr, int value){
-        int index = -1;
+    private void adjustTop(int[] arr, int value){
+        int index = arr.length;
         for(int x = 2; x >=0; x--){
             if(value <= arr[x]){
                 break;
             }
             index = x;
         }
-        if(index != -1){
-            while(index < arr.length){
-                int temp2 = arr[index];
-                arr[index] = value;
-                index++;
-                value = temp2;
-            }
+
+        while(index < arr.length){
+            int temp2 = arr[index];
+            arr[index] = value;
+            index++;
+            value = temp2;
         }
     }
 
